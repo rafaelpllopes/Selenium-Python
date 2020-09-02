78fab0bd216437865938b2582d24e1e575ce8049
rafaelpllopes <rafael.pllopes@gmail.com>
2020-09-01 21:48:23 -0300

    Aula 15, selenium docker

---
+++ aula 12/docker-selenium.py
@@ -0,0 +1,14 @@
+from selenium.webdriver import Remote
+
+"""
+    docker run -d -p 4444:4444 selenium/standalone-firefox
+    docker network create grid
+    docker run -d -p 4444:4444 --net grid --name selenium-hub selenium/hub:3.141.59
+    docker run -d --net grid -e HUB_HOST=selenium-hub selenium/nod-firefox:3.141.59
+"""
+
+driver = Remote(desired_capabilities={
+    'browserName': 'firefox'
+})
+
+driver.get('https://google.com')
